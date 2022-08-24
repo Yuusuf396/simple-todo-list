1bc8fb65660326fbb42aaab7c015caef72a908e3
ayomide <adebayoayomide396@gmail.com>
2022-08-24 20:55:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# simple-todo-list
+A simple Todo-list 
+
+
+#Technologies used :
+*Html
+*Css
+*Vannila JavaScript
+
+
+#Functions of the todo-list
+It accepts text ,
+Pushes it to the DIV,
+Saves the text in the localstorage,
+The delete button removes the text from the local storage
---
+++ script.js
@@ -0,0 +1,56 @@
+// 'use strict';  
+const inputText = document.getElementById('input_text');
+const add = document.querySelector('#button_add');
+const tasks = document.getElementById('tasks');
+
+
+ 
+tasks.innerHTML = localStorage.getItem('text');
+
+add.onclick = function () {
+    if (inputText.value === '')
+        alert('the message is not propperly filled')
+    else {
+        localStorage.setItem('text', tasks.innerHTML += ` 
+        
+           <div class="tasks ">     
+                <span class="text">
+                ${inputText.value}
+                </span>
+                <button class="tick">
+                <i class="fa-solid fa-check"></i>
+                </button>     
+                <button class="delete">
+                <i class="fa-solid fa-trash-can"></i>
+                </button>    
+    
+             </div>  
+            ` );
+         
+        tasks.innerHTML = localStorage.getItem('text');
+
+        const deleteBtn = document.querySelectorAll(".delete");
+        for (let i = 0; i < deleteBtn.length; i++) {
+            deleteBtn[i].addEventListener('click', function () {
+                this.parentNode.remove();
+                localStorage.removeItem('text');
+            })
+        }
+        
+        const tasksCross = document.querySelectorAll('.tasks');
+        for (let i = 0; i < tasksCross.length; i++) {
+            tasksCross[i].onclick = function () {
+                
+                this.classList.toggle("completed")
+            }
+        }
+         
+       
+        inputText.value = '';
+    }
+};
+
+
+
+
+
